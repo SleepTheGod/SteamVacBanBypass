d95fbc75d3820a02628bb43097fa88c609ea6a8b
ClumsyLulz <86472964+SleepTheGod@users.noreply.github.com>
2024-10-03 03:37:53 -0400

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <Windows.h>
+#include <string>
+#include <vector>
 
-// Utility function declarations
+// Advanced utility function declarations
 PVOID Utils_findPattern(PCWSTR moduleName, PCSTR pattern, SIZE_T patternSize);
 VOID Utils_hookImport(PCWSTR moduleName, PCSTR libraryName, PCSTR functionName, PVOID newFunction);
+std::vector<std::string> Utils_getLoadedModules();
+void Utils_logMessage(const char* format, ...);
